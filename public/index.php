95697250c456c458930a0970278671a0224a1f16
Samuel Woldegiorgies <samuel.woldegiorgies@icloud.com>
2025-11-22 15:34:02 +0300

    Fix index.php and hide deprecation warnings

--- public/index.php
+++ public/index.php
@@ -2,10 +2,10 @@
 
 use Illuminate\Contracts\Http\Kernel;
 use Illuminate\Http\Request;
+error_reporting(E_ALL & ~E_DEPRECATED & ~E_USER_DEPRECATED);
 
 define('LARAVEL_START', microtime(true));
-cd /Users/samic/Desktop/Code-Projects/habeshair
-php artisan serve
+
 
 /*
 |--------------------------------------------------------------------------
